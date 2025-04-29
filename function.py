e25b38919ace9ed604d5181b543e825fd3c52824
Matheus Buje Pegorin <00001111117160sp@aluno.educacao.sp.gov.br>
2025-04-29 10:19:14 -0300

    primeiro commit soma e multiplicacao

---
+++ function.py
@@ -0,0 +1,16 @@
+def multiplicacao():
+    num_01 = 25
+    num_02 = 31
+
+    resultado = num_01 + num_02
+
+    return resultado
+
+print(multiplicacao())
+
+def multiplicarDoisNumeros(nota_01, nota_02):
+    resultado = nota_01 * nota_02
+
+    return print(resultado)
+
+# somarDoisNumeros(24, 98)
